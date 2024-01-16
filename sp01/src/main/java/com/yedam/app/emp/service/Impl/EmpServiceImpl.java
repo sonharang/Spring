5d374eb5fbcc490816e05714b50b1542761558a3
sonharang <bhson3@naver.com>
2024-01-16 13:52:05 +0900

    3일차 진행중

---
+++ sp01/src/main/java/com/yedam/app/emp/service/Impl/EmpServiceImpl.java
@@ -0,0 +1,51 @@
+package com.yedam.app.emp.service.Impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yedam.app.emp.mapper.EmpMapper;
+import com.yedam.app.emp.service.EmpService;
+import com.yedam.app.emp.service.EmpVO;
+
+@Service
+public class EmpServiceImpl implements EmpService {
+	
+	@Autowired
+	EmpMapper empMapper;
+	
+	@Override
+	public List<EmpVO> getEmpAll() {
+		return empMapper.selectEmpList();
+	}
+
+	@Override
+	public EmpVO getEmpInfo(EmpVO empVO) {
+		return empMapper.selectEmpInfo(empVO);
+	}
+
+	@Override
+	public int insertEmpInfo(EmpVO empVO) {
+		int result = empMapper.insertEmpInfo(empVO);
+		if(result == 1) {
+			return empVO.getEmployeeId();
+		}else {
+			return -1;			
+		}
+	}
+
+	@Override
+	public Map<String, Object> updateEmpInfo(EmpVO empVO) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		return map;
+	}
+
+	@Override
+	public boolean deleteEmpInfo(int empId) {
+		return false;
+	}
+
+}
